d97eb7a6e55d9c69b4e0451f901be98e7741c734
Студент группы IS <is@sielom.local>
2025-04-16 11:21:20 +0500

    Соединение с supabase

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+const { createClient } = require('@supabase/supabase-js')
+
+const supabase = createClient('https://fjmitbmygewclwkmgttb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqbWl0Ym15Z2V3Y2x3a21ndHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NzY2ODEsImV4cCI6MjA2MDM1MjY4MX0.65Wy5XDp9WQWCSpKF7sB9Egd0BG-ehkb5tsww4c2WwA')
+
 const express = require('express')
 const color = require('colorette')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/students', async (req, res) => {
+
+  const { data, error } = await supabase
+  .from('students')
+  .select()
+
+  res.json(data)
+})
+
+app.post('/students', (req, res) => {
+  res.send('Добавление студентов')
+})
+
+app.delete('/students/:id', async (req, res) => {
+
+  const response = await supabase
+  .from('students')
+  .delete()
+  .eq('id', req.params.id)
+
+  res.json('Студент с id = ' + req.params.id + 'удален!')
+})
+
+app.put('/students', (req, res) => {
+  res.send('Изменение студента')
 })
 
 app.listen(port, () => {
